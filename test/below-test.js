5e4753356c9f129ff8015e3c6385b187087760e6
starcolon <tao@starcolon.com>
2015-05-05 18:47:22 +0700

    INITIAL BLANK TEST SCRIPT

---
+++ test/below-test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
+chai.should();
+chai.use(require('chai-things'));
+chai.config.includeStack = true;
+
+"use strict";
+
