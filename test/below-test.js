89baa8b2b97a7c562386f56776ffa8cc6f4aa999
starcolon <tao@starcolon.com>
2015-05-05 18:58:08 +0700

    FIRST TEST CASE

--- test/below-test.js
+++ test/below-test.js
@@ -7,3 +7,23 @@ chai.config.includeStack = true;
 
 "use strict";
 
+// Module dependency
+var below = require('../below.js');
+
+describe('@below test kit',function(){
+
+	describe('Fundamental tests', function(){
+
+		it('should create a settings object', function(){
+			var settings = below.settings.create();
+
+			expect(settings).to.have.property('size');
+			expect(settings).to.have.property('entrances');
+			expect(settings).to.have.property('exits');
+			expect(settings).to.have.property('items');
+			expect(settings).to.have.property('obstacles');
+			expect(settings).to.have.property('walls');
+		})
+
+	})
+})
